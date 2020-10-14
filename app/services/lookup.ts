f6b364a64e136d3fd96d04f9db30560c216aa896
Andrew Yang <andrew.yang@rokt.com>
2020-10-14 19:12:21 +1100

    lookup jira

--- app/services/lookup.ts
+++ app/services/lookup.ts
@@ -1,16 +1,26 @@
 import { getTicketDetails } from "./jira/jira";
 
-export const lookup = async(body, type, value) => {
+export const lookup = async (body, type, value) => {
+  switch (type.toLowerCase()) {
+    case 'jira':
+      return await handleJira(value);
+    case 'account':
+      return await handleAccount(value);
+  }
+}
 
-  if (!new RegExp('([a-zA-Z]{2,4}-\\d+)', 'g').test(value)) {
+const handleJira = async (ticketNumber) => {
+  if (!new RegExp('([a-zA-Z]{2,4}-\\d+)', 'g').test(ticketNumber)) {
     return {
       text: 'Invalid ticket number!',
     };
   }
-  console.log(value);
-  const ticketDetails = await getTicketDetails(value);
-  console.log(ticketDetails);
-  return {
-    text: JSON.stringify(ticketDetails),
-  };
+  console.log(ticketNumber);
+  const ticketDetails = await getTicketDetails(ticketNumber);
+  console.log(ticketDetails.data.fields.status, ticketDetails.data.summary);
+  return { text: JSON.stringify(ticketDetails.data.fields.status) };
+}
+
+const handleAccount = async (accountId) => {
+
 }
