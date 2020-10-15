34723e4fbf259da5bcb3801a03c1e48eebdfcbed
Jonathan <jonathan.he@rokt.com>
2020-10-15 12:13:38 +1100

    restructure projects

--- app/services/cron/cron.ts
+++ app/services/cron/cron.ts
@@ -1,5 +1,5 @@
 import * as cron from 'cron'
-import { isTicketsStatusChanged } from "../jira/jira-status";
+import { isTicketsStatusChanged } from "./jira-job";
 
 const job = new cron.CronJob('*/10 * * * * *', async function () {
     console.log('*** CronJob is running ***')
